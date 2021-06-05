69e1c6fa57a565f79c186fdc6196a31c18ead885
Manfred Steyer <manfred.steyer@gmx.net>
2021-06-05 19:34:27 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>
+      {{ msg }}
+      <img alt="Vue logo" src="https://cli.vuejs.org/favicon.png" height="30">
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {  
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
